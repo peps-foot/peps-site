f6d790ed3739c0a6ab1c7a63c3ec8feecd56c018
peps-foot <peps.foot@gmail.com>
2025-04-24 00:30:02 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        violetPeps: '#7e22ce',
+        goldPeps: '#facc15',
+        darkText: '#1e293b',
+        lightBg: '#f8fafc',
+      },
+    },
+  },
+  plugins: [],
+}
