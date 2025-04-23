98adaac7c0fed79000f6108f6e0b2759bbb5070c
peps-foot <peps.foot@gmail.com>
2025-04-24 01:07:34 +0200

    Delete classement.js

--- classement.js
+++
@@ -1,8 +0,0 @@
-export default function Classement() {
-  return (
-    <div className="min-h-screen bg-white p-8">
-      <h1 className="text-2xl font-semibold text-gray-800">Classement</h1>
-      <p className="text-gray-600 mt-2">Les classements seront visibles ici !</p>
-    </div>
-  );
-}
