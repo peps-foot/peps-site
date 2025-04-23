a0d467276bfae0d449f1213c50d01b87bac3c417
peps-foot <peps.foot@gmail.com>
2025-04-24 01:07:48 +0200

    Delete grilles.js

--- grilles.js
+++
@@ -1,8 +0,0 @@
-export default function Grilles() {
-  return (
-    <div className="min-h-screen bg-white p-8">
-      <h1 className="text-2xl font-semibold text-gray-800">Grilles de match</h1>
-      <p className="text-gray-600 mt-2">Bientôt disponibles ici...</p>
-    </div>
-  );
-}
