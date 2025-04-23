ebbcf6a2fa95e7f27168d2969c4ee21b84925403
peps-foot <peps.foot@gmail.com>
2025-04-24 01:59:07 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+// app/layout.tsx
+export const metadata = {
+  title: "PEPS - Pronos entre PoteS",
+  description: "Jeu de pronostics football gratuits entre amis",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
