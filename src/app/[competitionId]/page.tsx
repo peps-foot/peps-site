0d963d5db99aef697052414282cedfde3d333b24
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-06-08 16:23:09 +0200

    fix: test build with lowercase competitionId

--- src/app/[competitionId]/page.tsx
+++ src/app/[competitionId]/page.tsx
@@ -1,25 +1,3 @@
-// src/app/[competitionId]/page.tsx
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies, headers } from 'next/headers'
-import { redirect } from 'next/navigation'
-
-export default async function Page({ params }: { params: { competitionId: string } }) {
-  const supabase = createServerComponentClient({ cookies, headers });
-
-  const {
-    data: { session },
-    error: errSession
-  } = await supabase.auth.getSession();
-
-  if (!session || errSession) {
-    return redirect('/connexion');
-  }
-
-  const { competitionId } = params;
-
-  return (
-    <main>
-      <h1>Compétition : {competitionId}</h1>
-    </main>
-  );
+export default function Page({ params }: { params: { competitionId: string } }) {
+  return <h1>Compétition : {params.competitionId}</h1>;
 }
