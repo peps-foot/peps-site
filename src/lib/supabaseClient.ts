a6cde0d20266aa91e91f4b553b54c516e66ebff3
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-06-09 21:27:33 +0200

    remove dynamic page for test

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,4 +2,7 @@
 
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
-export const supabase = createBrowserSupabaseClient()
+export const supabase = createBrowserSupabaseClient({
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+})
