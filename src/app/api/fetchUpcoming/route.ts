b3179c52b93520418dff355dc10d432c7cd960e0
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-06-09 18:06:41 +0200

    remove dynamic page for test

--- src/app/api/fetchUpcoming/route.ts
+++ src/app/api/fetchUpcoming/route.ts
@@ -3,8 +3,8 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const SUPABASE_URL = process.env.SUPABASE_URL!
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!
 const API_KEY = process.env.API_FOOTBALL_KEY!
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
