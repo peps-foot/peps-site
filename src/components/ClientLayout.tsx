7f6033f6e090d51579de1151c82cf613bef8a359
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-06-11 15:07:29 +0200

    💡 Inject supabase env vars statically

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,12 +1,26 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 import OnlyClient from './OnlyClient'
 import SupabaseProvider from './SupabaseProvider'
 import { NavBar } from './NavBar'
-import { usePathname } from 'next/navigation'
 
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname() ?? ''
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const pathname = usePathname()
+  const [hasMounted, setHasMounted] = useState(false)
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  // 🔒 Ne rien afficher côté serveur ou avant montage
+  if (!hasMounted) return null
+
   const hideNavbarRoutes = ['/connexion', '/inscription', '/admin/grids']
   const showNavbar = !hideNavbarRoutes.includes(pathname)
 
