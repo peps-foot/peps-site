bb2056225e856454046b227fc2f026ec5f5b10fe
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-06-11 11:01:07 +0200

    force redeploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.exprContextCritical = false
+    return config
+  },
 }
 
 module.exports = nextConfig
