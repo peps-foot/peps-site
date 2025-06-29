eb800fd176221896ca40df0e50321090f61df358
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-06-29 22:24:45 +0200

    MAJ + règles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
   const router = useRouter()
+  const [isRedirecting, setIsRedirecting] = useState(true)
   
   useEffect(() => {
     // Redirige vers la seule compétition active
     router.replace('/connexion')
   }, [router])
 
+    if (isRedirecting) return null
   return null
 }
