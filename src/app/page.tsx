b52a678721fb44e489af997a358eb5eae7826779
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-07-19 11:52:15 +0200

    MAJ PTS all joueurs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,20 @@ import { supabase } from '../utils/supabase' // adapte le chemin si besoin
 export default function Home() {
   const router = useRouter()
   
-useEffect(() => {
-  const check = async () => {
-    const { data: { session } } = await supabase.auth.getSession();
-    if (!session) router.replace('/connexion');
-  };
-  check();
-}, []);
+  useEffect(() => {
+    const check = async () => {
+      if (typeof window === 'undefined') return;
+      const pathname = window.location.pathname;
 
+      // 🛑 Ne redirige pas si on est sur la page de réinitialisation
+      if (pathname === '/reset-password') return;
+
+      const { data: { session } } = await supabase.auth.getSession();
+      if (!session) router.replace('/connexion');
+    };
+
+    check();
+  }, []);
 
   return null
 }
