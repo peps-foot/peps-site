f6e3868546e666495daa98f377a8d3430f87bd6f
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-07-19 17:07:06 +0200

    reset-password

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -57,7 +57,7 @@ export default function ConnexionPage() {
     }
 
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: 'https://www.peps-foot.com/reset-password',
+        redirectTo: 'https://www.peps-foot.com/reset-password?flowType=implicit',
       });
     if (error) {
       setErrorMsg(error.message);
