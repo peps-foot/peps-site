9ed37714073d70e5126e701c2a413584ec5e7b88
Yann.TAFFOREAU <TAFFOREAU@petitval.fr>
2025-11-11 18:30:25 +0100

    notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -47,8 +47,8 @@ if (!self.__PEPS_BG_BOUND__) {
 
       const title = n.title || d.title || 'PEPS';
       const body  = n.body  || d.body  || '';
-      const icon  = toAbs(n.icon || d.icon || '/images/peps-notif-icon-192.png');
-      const badge = toAbs('/images/peps-badge-72.png');
+      const icon  = toAbs(n.icon || d.icon || '/images/notifications/empty-box.png');
+      const badge = toAbs('/images/notifications/peps-badge-72.png');
       const url   = d.url ? toAbs(d.url) : self.location.origin + '/';
       const tag   = d.tag   || 'peps-broadcast';
 
